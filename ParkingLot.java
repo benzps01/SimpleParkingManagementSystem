ca29e35b2ad4ec7b29a9aa75feb868070fb285ad
benzps01 <benzps01@gmail.com>
2024-02-17 23:43:09 +0530

    initialize parkinglot class

---
+++ ParkingLot.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkingLot {
+    String parkingLotId;
+    List<List<Slot>> slots;
+
+    ParkingLot(String parkingLotId, int nfloors, int noOfSlotsPerFir) {
+
+    }
+}
