1d05cc1d0bcbde698092f581fde5ef559b8d81df
benzps01 <benzps01@gmail.com>
2024-02-20 23:41:51 +0530

    added unPark, display Open and Occupied slots

--- ParkingLot.java
+++ ParkingLot.java
@@ -38,13 +38,55 @@ public class ParkingLot {
         return null;
     }
 
-    private String generateTicketId(int flr, int slno) {
-        return parkingLotId + "_" + flr + "_" + slno;
-    }
-
     public void unPark(String ticketId) {
         String[] extract = ticketId.split("_");
         int flr_idx = Integer.parseInt(extract[1]) - 1;
         int slot_idx = Integer.parseInt(extract[2]) - 1;
+
+        for (int i = 0; i < slots.size(); i++) {
+            for (int j = 0; j < slots.get(i).size(); j++) {
+                if (i == flr_idx && j == slot_idx) {
+                    Slot slot = slots.get(i).get(j);
+                    slot.vehicle = null;
+                    slot.ticketId = null;
+                    System.out.println("Unparked vehicle");
+                }
+            }
+        }
+    }
+
+    private String generateTicketId(int flr, int slno) {
+        return parkingLotId + "_" + flr + "_" + slno;
+    }
+
+    int getNoOfOpenSlots(String type) {
+        int count = 0;
+        for (List<Slot> floor : slots) {
+            for (Slot slot : floor) {
+                if (slot.vehicle == null && slot.type.equals(type))
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    void displayOpenSlots(String type) {
+        for (int i = 0; i < slots.size(); i++) {
+            for (int j = 0; j < slots.get(i).size(); j++) {
+                Slot slot = slots.get(i).get(j);
+                if (slot.vehicle == null && slot.type.equals(type))
+                    System.out.println("Floor " + (i + 1) + " slot " + (j + 1));
+            }
+        }
+    }
+
+    void displayOccupiedSlots(String type) {
+        for (int i = 0; i < slots.size(); i++) {
+            for (int j = 0; j < slots.get(i).size(); j++) {
+                Slot slot = slots.get(i).get(j);
+                if (slot.vehicle != null && slot.type.equals(type))
+                    System.out.println("Floor " + (i + 1) + " slot " + (j + 1));
+            }
+        }
     }
 }
